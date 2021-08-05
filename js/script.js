1fea822bcfb04947a8ebc3c03fe41de5e1522794
anderson.batista <andersonwdf@gmail.com>
2021-08-05 18:35:12 -0300

    Criando função start

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ var globalNames = ['Um', 'Dois', 'Tres', 'Quatro', 'Cinco'];
 var inputName = null;
 var isEditing = false;
 var currentIndex = null;
+
+function start() {
+  inputName = document.querySelector('#inputName');
+
+  preventFormSubmit();
+  activateInput();
+  render();
+}
